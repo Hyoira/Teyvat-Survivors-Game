922c8e49d1aa61fe22b8674bd11949172fb8be9a
Hyoira <yukkurahirokura@gmail.com>
2025-04-19 14:50:00 +0900

    update 25041903

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,8 +2,8 @@ const cacheName = "Hyoira-Teyvat-Survivors-1.1";
 const contentToCache = [
     "Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js",
     "Build/fe83b67b59f07f2ea0a473848c3a344c.framework.js",
-    "Build/9fb8430d7f82d133a67c350010abedfa.data",
-    "Build/34365bb2feb7f6e8265e550f894b2982.wasm",
+    "Build/4560446b7d0ece897c248087b099635c.data",
+    "Build/3c869884c9f01f2b40a85e521900f10a.wasm",
     "TemplateData/style.css"
 
 ];
